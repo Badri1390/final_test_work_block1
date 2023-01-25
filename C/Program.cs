04de09d2c420792fedada37b943ec6b12fe11e5c
Badri <9299547077@mail.ru>
2023-01-25 22:26:20 +0300

    C#

---
+++ C/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Clear();
+string[] array = { "hello", "2", "world", ";-)", "H" };
+System.Console.WriteLine($"[{String.Join(" ; ", new_array(array))}]");
+
+
+
+string[] new_array (string[] array)
+{
+    string[] res = { };
+    foreach (var item in array)
+    {
+        if (item.Length > 0 && item.Length <= 3)
+        {
+            res = res.Append(item).ToArray();
+        }
+    }
+    return res;
+}
